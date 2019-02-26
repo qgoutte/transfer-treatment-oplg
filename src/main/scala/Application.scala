f90afbaacd7281e264193f3b1588aedab7bd7cf5
esuau <evan.suau@etu.u-pec.fr>
2019-02-27 00:14:18 +0100

    feat(heartbeat): Implement Akka HTTP webserver with Routing DSL

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,3 +1,34 @@
-object Application extends App {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object Application {
+
+  def main(args: Array[String]) {
+
+    implicit val system: ActorSystem = ActorSystem("my-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+    val route =
+      path("heartbeat") {
+        get {
+          complete(HttpEntity(ContentTypes.`application/json`, """{ "code": 200, "status": "UP AND RUNNING" }"""))
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
 
 }
