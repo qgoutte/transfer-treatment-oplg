93588ad246b74c05d477646342a492aa1f157c32
esuau <evan.suau@etu.u-pec.fr>
2019-03-13 00:48:13 +0100

    feat(database): Read methods return Transfer or Seq results

--- src/main/scala/service/DatabaseService.scala
+++ src/main/scala/service/DatabaseService.scala
@@ -1,8 +1,14 @@
 package service
 
+import actor.Transfer
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.bson.codecs.configuration.CodecRegistry
 import org.mongodb.scala._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.mongodb.scala.bson.codecs.Macros._
 import org.mongodb.scala.model.Filters._
 import org.mongodb.scala.model.Updates._
+import util.Helpers._
 
 object DatabaseService {
 
@@ -10,30 +16,32 @@ object DatabaseService {
   // To directly connect to the default server localhost on port 27017
   val mongoClient: MongoClient = MongoClient()
 
+  val codecRegistry: CodecRegistry = fromRegistries(fromProviders(classOf[Transfer]), DEFAULT_CODEC_REGISTRY )
+
   // WARNING DB needs "transfers-db" as name
-  val database: MongoDatabase = mongoClient.getDatabase("transfers-db")
+  val database: MongoDatabase = mongoClient.getDatabase("transfers-db").withCodecRegistry(codecRegistry)
 
   // WARNING COLLECTION needs "transfers" as name
-  val collection: MongoCollection[Document] = database.getCollection("transfers")
+  val collection: MongoCollection[Transfer] = database.getCollection("transfers")
 
   //FUNCTIONS
   //Return all transfers
-  def getAllTransfers(): Unit = {
-    collection.find()
+  def getAllTransfers(): Seq[Transfer] = {
+    collection.find().results()
   }
 
   //Return one transfer with this id
-  def getTransferById(id: Integer): Unit = {
-    collection.find(equal("id", id))
+  def getTransferById(id: Integer): Transfer = {
+    collection.find(equal("id", id)).first().headResult()
   }
 
   //Return all documents with the status in parameter
-  def getTransfersByStatus(status: String): Unit = {
-    collection.find(equal("status", status))
+  def getTransfersByStatus(status: String): Seq[Transfer] = {
+    collection.find(equal("status", status)).results()
   }
 
   //Add one transfer
-  def addTransfer(doc: Document): Unit = {
+  def addTransfer(doc: Transfer): Unit = {
     collection.insertOne(doc)
   }
 
@@ -46,4 +54,5 @@ object DatabaseService {
   def deleteTransfer(id: Integer): Unit = {
     collection.deleteOne(equal("id", id))
   }
+
 }
