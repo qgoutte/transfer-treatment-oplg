b28a2a6423defb10e30136a8ad4529767dff4c68
esuau <evan.suau@etu.u-pec.fr>
2019-03-12 23:28:43 +0100

    refactor(database): Perform code cleaning and reformatting

--- src/main/scala/database/TransferDatabase.scala
+++ src/main/scala/database/TransferDatabase.scala
@@ -14,36 +14,36 @@ class TransferDatabase {
   val database: MongoDatabase = mongoClient.getDatabase("transfers-db")
 
   // WARNING COLLECTION needs "transfers" as name
-  val collection: MongoCollection[Document] = database.getCollection("transfers");
+  val collection: MongoCollection[Document] = database.getCollection("transfers")
 
   //FUNCTIONS
   //Return all transfers
-  def getAllTransfers(): Unit ={
-    return collection.find()
+  def getAllTransfers(): Unit = {
+    collection.find()
   }
 
   //Return one transfer with this id
-  def getTransferById(id:Integer):Unit={
-    return collection.find(equal("id",id))
+  def getTransferById(id: Integer): Unit = {
+    collection.find(equal("id", id))
   }
 
   //Return all documents with the status in parameter
-  def getTransfersByStatus(status:String):Unit = {
-    return collection.find(equal("status",status))
+  def getTransfersByStatus(status: String): Unit = {
+    collection.find(equal("status", status))
   }
 
   //Add one transfer
-  def addTransfer(doc : Document): Unit ={
+  def addTransfer(doc: Document): Unit = {
     collection.insertOne(doc)
   }
 
   //Update one transfer
-  def updateTransfer(id:Integer,status:String ):Unit={
+  def updateTransfer(id: Integer, status: String): Unit = {
     collection.updateOne(equal("id", id), set("status", status))
   }
 
   //Delete one transfer
-  def deleteTransfer(id:Integer):Unit={
+  def deleteTransfer(id: Integer): Unit = {
     collection.deleteOne(equal("id", id))
   }
 }
